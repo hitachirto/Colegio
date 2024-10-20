a42cdfca46d69edb12ff00899f95a4b459c5c982
Ronny Talcan <overload_2066@hotmail.com>
2024-10-20 01:15:35 -0300

    se ha programado la conxion a la base de datos "CONEXIONMAESTRA"

---
+++ conexion/CONEXIONMAESTRA.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.SqlClient
+
+Module CONEXIONMAESTRA
+    Public conexion As New SqlConnection("Data Source=DESKTOP-JUJ0HFL\SQLEXPRESS;Initial Catalog=BASECOLEGIO;Integrated Security=True")
+
+    Sub abrir()
+        If conexion.State = 0 Then
+            conexion.Open()
+
+        End If
+    End Sub
+    Sub cerrar()
+        If conexion.State = 1 Then
+            conexion.Close()
+
+        End If
+    End Sub
+
+End Module
