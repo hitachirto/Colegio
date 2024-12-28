c68dffc24af85f03b6b2ba983484cb48e8740d45
Ronny Talcan <overload_2066@hotmail.com>
2024-12-28 12:02:26 -0300

    Revert "se agrego al menu principal el DataGridView de usuariosok"
    This reverts commit d495c8116d1be6fcf9cdc9d1493b2edc1657a284.

--- MODULOS/MENUPRINCIPAL/MENUPRINCIPALMATRICULAS.vb
+++ MODULOS/MENUPRINCIPAL/MENUPRINCIPALMATRICULAS.vb
@@ -10,8 +10,4 @@
     Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs)
 
     End Sub
-
-    Private Sub MENUPRINCIPALMATRICULAS_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
