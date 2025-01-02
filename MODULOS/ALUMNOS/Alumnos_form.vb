a786bc13787fb46f05f1cca748e237a2a96a7333
Ronny Talcan <overload_2066@hotmail.com>
2025-01-02 01:31:46 -0300

    se programo boton eliminar de datalistado alumnos el cual tacha los alumnos mas no elimina los registros.

--- MODULOS/ALUMNOS/Alumnos_form.vb
+++ MODULOS/ALUMNOS/Alumnos_form.vb
@@ -29,9 +29,17 @@ Public Class Alumnos_form
             Multilinea(datalistado)
             datalistado.Columns(1).Visible = False
 
-        Catch ex As Exception : MessageBox.Show(ex.Message)
 
+
+        Catch ex As Exception : MessageBox.Show(ex.Message)
         End Try
+        For Each row As DataGridViewRow In datalistado.Rows
+            If row.Cells("Estado").Value = "ELIMINADO" Then
+                row.DefaultCellStyle.Font = New Font("segoe UI", 10, FontStyle.Strikeout Or FontStyle.Bold)
+                row.DefaultCellStyle.ForeColor = Color.Red
+            End If
+        Next
+
     End Sub
     Private Sub GUARDARToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GUARDARToolStripMenuItem.Click
         Try
@@ -43,6 +51,7 @@ Public Class Alumnos_form
             cmd.Parameters.AddWithValue("@Apellido_Materno", txtapellidoMaterno.Text)
             cmd.Parameters.AddWithValue("@Nombres", txtnombres.Text)
             cmd.Parameters.AddWithValue("@Nro_de_documento", txtdocumento.Text)
+            cmd.Parameters.AddWithValue("@Estado", "ACTIVO")
             cmd.ExecuteNonQuery()
             cerrar()
             MOSTRAR()
@@ -104,5 +113,36 @@ Public Class Alumnos_form
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Panel4.Visible = False
+    End Sub
+
+    Private Sub datalistado_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles datalistado.CellClick
+        If e.ColumnIndex = Me.datalistado.Columns.Item("Eli").Index Then
+            Dim result As DialogResult
+            result = MessageBox.Show("¿realmente desea eliminar a este alumno?", "Eliminando Registros", MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+            If result = DialogResult.OK Then
+                Dim cmd As SqlCommand
+                Try
+                    For Each row As DataGridViewRow In datalistado.SelectedRows
+                        Dim idalumnoSeleccionado As Integer = Convert.ToInt32(row.Cells("idalumno").Value)
+                        Try
+                            abrir()
+                            cmd = New SqlCommand("Eliminar_alumno", conexion)
+                            cmd.CommandType = 4
+                            cmd.Parameters.AddWithValue("@idalumno", idalumnoSeleccionado)
+                            cmd.ExecuteNonQuery()
+                            cerrar()
+
+                        Catch ex As Exception
+                            MessageBox.Show(ex.Message)
+
+                        End Try
+                    Next
+                    Call MOSTRAR()
+
+                Catch ex As Exception
+
+                End Try
+            End If
+        End If
     End Sub
 End Class
